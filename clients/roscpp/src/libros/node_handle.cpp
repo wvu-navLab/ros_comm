2833f4a777938c42f3dabe3e951b00d5e62af202
Teresa Davies <teresa.davies@kinnami.com>
2022-05-16 14:39:23 -0600

    Make use of namespaces to be consistent with expected ROS usage

--- clients/roscpp/src/libros/node_handle.cpp
+++ clients/roscpp/src/libros/node_handle.cpp
@@ -555,7 +555,15 @@ void NodeHandle::shutdown()
 #if AMISHARE_ROS == 1
 void NodeHandle::setTopicRemote(const std::string& topic)
 {
-  std::string param_name = topic + "_global";
+  std::string param_name;
+  if (topic[0] == '/')
+  {
+    param_name = topic + "_global";
+  }
+  else
+  {
+    param_name = namespace_ + "/" + topic + "_global";
+  }
   setParam(param_name, true);
 }
 
