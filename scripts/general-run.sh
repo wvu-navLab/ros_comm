a16c293d19a7d3a87ce212191c15158e6c6d99f6
Teresa Davies <teresa.davies@kinnami.com>
2023-02-01 15:47:53 -0700

    Correction to run script to work with AmiNotify

--- scripts/general-run.sh
+++ scripts/general-run.sh
@@ -20,7 +20,6 @@ export ROS_MASTER_URI=http://$LOCAL_IP:11311; export ROS_HOSTNAME=$LOCAL_IP
 source $SCRIPT_PATH/amishare-ros-variables.sh
 export FIFO_PATH=/tmp/com_kinnami_amishare_BoxAFSNotify_AmiShareFS-$USER
 export REG_FILE=$AMISHARE_PREFIX/registration
-touch $REG_FILE
 
 roscore -r $REG_FILE > $OUTPUT_PATH/roscoreout.txt 2>&1 &
 echo $! > $SCRIPT_PATH/test-pids.txt
@@ -29,6 +28,8 @@ sleep 1 # roscore needs a moment to start up
 python3 $AMINOTIFY $FIFO_PATH $SOCKET_PATH $REG_FILE $AMISHARE_PREFIX > $OUTPUT_PATH/aminotifyout.txt 2>&1 &
 echo $! >> $SCRIPT_PATH/test-pids.txt
 sleep 1
+touch $REG_FILE
+echo "" >> $REG_FILE
 
 for name in $ARGS_LIST
 do if [ $name != $PACKAGE_NAME ]
